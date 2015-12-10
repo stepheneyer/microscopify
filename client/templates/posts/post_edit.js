96923cb390026bf845265b8875a2bf358680d853
Stephen Eyer <stephen@stepheneyer.com>
2015-12-10 09:44:16 -0800

    Basic error reporting

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -7,7 +7,7 @@ Template.postEdit.events({
     var postProperties = {
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
-    }
+    };
 
     Posts.update(currentPostId, {$set: postProperties}, function(error) {
       if (error) {
